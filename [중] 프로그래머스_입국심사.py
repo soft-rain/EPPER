9ee03b9d442e90f4c67dea165daefa01fb815620
softrain <yurimplus@naver.com>
2021-09-10 21:16:21 +0900

    [중] 프로그래머스 입국심사

--- [중] 프로그래머스_입국심사.py
+++ [중] 프로그래머스_입국심사.py
@@ -1,7 +1,7 @@
 def solution(n, times):
     answer = 9999999999999
     times.sort()
-    start = 1
+    start = 1  # 최소 시간 변수
     end = n * times[len(times) - 1]  # 최대 심사 시간 * n명
     temp = 0
 
@@ -11,7 +11,7 @@ def solution(n, times):
         temp = 0  # mid 시간 동안 심사 가능한 사람의 수
 
         for i in range(len(times)):
-            temp += mid // times[i]
+            temp += mid // times[i]  # 총 시간을 심사관들의 심사 시간으로 나눈 각각 i번째 심사관이 심사할 수 있는 사람의 수
         if temp >= n:
             answer = min(answer, mid)
             end = mid - 1
