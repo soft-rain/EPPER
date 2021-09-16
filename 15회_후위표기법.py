eaeda6645cfa0d43079a2bdbe462d6027972232c
softrain <yurimplus@naver.com>
2021-09-17 03:30:07 +0900

    15회 EPPER 후위표기법

---
+++ 15회_후위표기법.py
@@ -0,0 +1,31 @@
+def solution(m, a):
+    stack = []
+    alist = a.split()
+    for i in alist:
+        print(stack)
+
+        if i == "+":
+            op2 = stack.pop()
+            op1 = stack.pop()
+            stack.append(op1 + op2)
+        elif i == "-":
+            op2 = stack.pop()
+            op1 = stack.pop()
+            stack.append(op1 - op2)
+        elif i == "*":
+            op2 = stack.pop()
+            op1 = stack.pop()
+            stack.append(op1 * op2)
+        elif i == "/":
+            op2 = stack.pop()
+            op1 = stack.pop()
+            stack.append(op1 // op2)
+        else:
+            stack.append(int(i))
+    return int(stack.pop())
+
+
+m = int(input())
+a = input()
+answer = solution(m, a)
+print(answer)
