fff45cc17f6808ae64bfc0c3f5561813d52eeec8
softrain <yurimplus@naver.com>
2021-09-17 18:29:54 +0900

    [하] 백준 13458 시험감독

--- [하] BOJ13458_시험감독.py
+++ [하] BOJ13458_시험감독.py
@@ -1,14 +1,22 @@
 N = int(input())  # 시험장의 개수
-arr = list(map(int, input().split()))  # 각 시험장에 있는 응시자 수
+A = list(map(int, input().split()))  # 각 시험장에 있는 응시자 수
 B, C = map(int, input().split())  # 총감독관이 감시할 수 있는 응시자 수, 부감독관이 감시할 수 있는 응시자 수
-count = 0
-for i in arr:
-    if i > 0:
-        i -= B
-        count += 1
-    if i > 0:
-        count += i // C
-        if i % C != 0:
-            count += 1
 
-print(count)
+
+def solution(N, A, B, C):
+    ans = 0
+    for i in range(N):
+        A[i] -= B  # 모든 시험장에서 총 감독관이 감시할 수 있는 응시자 수 빼기
+        ans += 1  # 총감독관 수 더함
+
+        if A[i] > 0:
+            ans += A[i] // C
+            if A[i] % C != 0:
+                ans += 1
+            else:
+                ans += 0
+
+    return ans
+
+
+print(solution(N, A, B, C))
